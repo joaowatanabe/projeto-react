8b6c3248c3846cbfcace191df0914b70fe57f00e
wabemusic <jvwatanabe@gmail.com>
2024-09-24 21:28:18 -0300

    primeiro componente - Botão inútil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import Botao from '../components/Botao'
 
 export default function Home() {
-  return 'teste';
+  return (
+    <div className='flex gap-2'>
+      <Botao label="Salvar"/>
+      <Botao label="Cancelar"/>
+    </div>
+  );
 }
---
+++ src/components/Botao.tsx
@@ -0,0 +1,6 @@
+// Primeiro componente
+export default function Botao(props: any) {
+  return (
+    <button className="bg-green-500 p-2 rounded-md">Botão inútil</button>
+  )
+}
