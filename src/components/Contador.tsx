310a6097696084d5556b240b9a14c7fcd86f0bb6
wabemusic <jvwatanabe@gmail.com>
2024-09-24 23:21:53 -0300

    Criação do numero estado

--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -1,12 +1,29 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Contador() {
-  let numero:number = 0
+  const estado = useState(0)
+  const numero = estado[0]
+  const alterarNumero = estado[1]
+
+  function decrementar() {
+    alterarNumero(numero - 1)
+   
+  }
+  
+  function incrementar() {
+    alterarNumero(numero + 1)
+   
+  }
+
   return (
     <div>
       <h1>Contador</h1>
       <span>{numero}</span>
       <div className="flex gap-2">
-        <button className="bg-red-500 p-2 rounded-md">Decrementar</button>
-        <button className="bg-green-400 p-2 rounded-md">Incrementar</button>
+        <button onClick={decrementar} className="bg-red-500 p-2 rounded-md">Decrementar</button>
+        <button onClick={incrementar} className="bg-green-400 p-2 rounded-md">Incrementar</button>
       </div>
     </div>
   )
